0a861bb9c5ab01fbac94a7267b0ac768fb19348c
Kah_0 <aitor.alonsocoloma2002@gmail.com>
2022-10-08 11:30:13 +0200

    test commit

--- Atom-AT/src/atom/at/AtomAT.java
+++ Atom-AT/src/atom/at/AtomAT.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package atom.at;
 
-/**
- *
- * @author aitor
- */
-public class AtomAT {
+import robocode.*;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class AtomAT extends Robot{
+
+    public void Run() {
+        ahead(100);
     }
     
 }
---
+++ Atom-AT/src/atom/at/doird01.java
@@ -0,0 +1,7 @@
+package atom.at;
+
+import robocode.*;
+
+public class doird01 extends AdvancedRobot{
+    
+}
